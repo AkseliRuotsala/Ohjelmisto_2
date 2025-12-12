8e7b1c10132561bd93562157f6f4bb561cf9d913
akseliruotsala <akseli.ruotsala@metropolia.fi>
2025-12-12 15:47:15 +0200

    mod3

--- Java_script/Js-tehtävät/Js-mod3 tehtävät/Tehtävä4/4.js
+++ Java_script/Js-tehtävät/Js-mod3 tehtävät/Tehtävä4/4.js
@@ -3,7 +3,7 @@ const students = [
     { name: 'John', id: '2345768' },
     { name: 'Paul', id: '2134657' },
     { name: 'Jones', id: '5423679' },
-]; // Jos tämä ei ole valmiina, lisää se.
+];
 
 for (let student of students) {
     const option = document.createElement('option');
